2b90c30683ca43c7e4aa7814ea6bed5349112246
Nel Vandenbrande <vdbnel@gmail.com>
2019-09-10 09:55:02 +0200

    Orry zijn stomme bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        View::share('takken', Tak::get());
+        // View::share('takken', Tak::get());
     }
 
     /**
