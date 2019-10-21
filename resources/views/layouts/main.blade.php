d1ed759eece2370b933b2af8a8027f8e5a0ed778
Orry Verpoort <orry@verpoort.be>
2019-10-21 09:30:03 +0200

    Revert parts of main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -105,6 +105,12 @@
                                 </li>
 
                                 <li class="nav__sublist-item">
+                                    <a href="/alle-info/lid-worden" class="nav__link nav__link--sublist">
+                                        Lid worden
+                                    </a>
+                                </li>
+
+                                <li class="nav__sublist-item">
                                     <a href="/alle-info/trooper" class="nav__link nav__link--sublist">
                                         Trooper
                                     </a>
@@ -280,7 +286,7 @@
                                 </li>
                             
                                 <li class="social__item">
-                                    <a href="mailto:fos207ste@gmail.com" class="social__link social__link--mail">
+                                    <a href="mailto:fos207ste@gmail.com" class="social__link social__link--mail" target="_blank">>
                                         <span class="fa--lg"><i class="fas fa-at"></i></span>
                                     </a>
                                 </li>
@@ -300,7 +306,8 @@
                                     <div class="col-8">
                                         <h5 class="contact__title">Eenheidsleidster</h5>
                                         <p>Marie 'Ara'<br>Lammertyn</p>
-                                        <a href="tel:0491089740">0491/08.97.40</a>
+                                        <a href="tel:0491089740" target="_blank">0491/08.97.40</a>
+                                        <a href="mailto:fos207ste@gmail.com" target="_blank">fos207ste@gmail.com</a>
                                     </div>
                                 </div>
                             </div>
