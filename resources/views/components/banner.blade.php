dc67821b4ab3dd34341d7cd8d2f54a01c102491e
Orry Verpoort <orry@verpoort.be>
2019-11-02 13:38:42 +0100

    Banner coloring

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,4 +1,4 @@
-<section class="section section--small-spacing banner">
+<section class="section section--small-spacing banner banner--{{ $banner->color }}">
 	<img src="{{ asset('/img/banners/' . $banner->color . '-' . $banner->pattern . '-' . $banner->strength . '.png') }}" alt="{{ $banner->alt }}" class="banner__image">
 
 	<h1 class="banner__title @if(isset($page_sub_title)) banner__title--with-sub-title @endif">
