74be6dd7a5ff7c1fc744a0fcdbd9504711258c7d
Orry Verpoort <orry@verpoort.be>
2019-11-15 17:40:20 +0100

    Section update with sections on content

--- resources/views/alle-info/kost_scouts.blade.php
+++ resources/views/alle-info/kost_scouts.blade.php
@@ -5,7 +5,7 @@
         <img src="{{ asset('/img/banner.jpg') }}" alt="Banner" class="banner__banner">
     </section>
 
-    <div class="row section">
+    <div class="row section section--small-spacing">
         <div class="col-12">
             <h2>Wat kost de scouts?</h2>
 
@@ -187,85 +187,101 @@
         <div class="col-12 col-md-5">
             <h4>Toelichting</h4>
 
-            <h5>
-                Lidgeld
-            </h5>
+            <div class="section section--small-spacing">
+                <h5>
+                    Lidgeld
+                </h5>
+                
+                <p>
+                    Het lidgeld is een jaarlijkse kost die iedereen die actief is in onze werking moet betalen. Het grootste deel hiervan gaat rechtstreeks naar onze koepel, FOS Open Scouting, dit ondersteunt hun werking en in ruil hiervoor ben je bij ons verzekerd voor lichamelijke ongevallen en burgerlijke aansprakelijkheid. Een klein deeltje hiervan gaat naar onze eenheid en bekostigd voor een stuk onze dagelijkse werking (materiaal, 4-uurtjes etc.).
+                </p>
+                
+                <p>
+                    De kost voor het lidgeld bedraagt 38 euro
+                </p>
+            </div>
 
-            <p>
-                Het lidgeld is een jaarlijkse kost die iedereen die actief is in onze werking moet betalen. Het grootste deel hiervan gaat rechtstreeks naar onze koepel, FOS Open Scouting, dit ondersteunt hun werking en in ruil hiervoor ben je bij ons verzekerd voor lichamelijke ongevallen en burgerlijke aansprakelijkheid. Een klein deeltje hiervan gaat naar onze eenheid en bekostigd voor een stuk onze dagelijkse werking (materiaal, 4-uurtjes etc.).
-            </p>
+            <div class="section section--small-spacing">
+                <h5>
+                    Bivak
+                </h5>
+                
+                <p>
+                    Al onze takken gaan jaarlijks op weekend, het bivak. Voor de bevers & de welpen is dit samen en valt dit in de paasvakantie. De JGV’s en OGV’s gaan apart en in tenten op bivak.
+                </p>
+                
+                <p>
+                    Voor alle groepen bedraagt de prijs hiervan typisch rond de 50 euro.
+                </p>
+            </div>
 
-            <p>
-                De kost voor het lidgeld bedraagt 38 euro
-            </p>
+            <div class="section section--small-spacing">
+                <h5>
+                    Zomerkamp
+                </h5>
+                
+                <p>
+                    Elke zomer gaan wij met alle groepen samen op kamp. Dit is het hoogtepunt van het jaar en zorgt bij iedereen voor onvergetelijke momenten. De welpen, JGV’s en OGV’s gaan 10 dagen. In het midden is er een dag waarop de ouders verwelkomd worden om het kampterrein te bezoeken, de ouderbezoekdag. Terwijl kunnen de ouders van de bevers hun kinderen brengen. Voor hen begint het kamp dan en duurt dit 5 dagen.
+                </p>
+                
+                <p>
+                    De prijs hiervoor bedraagt bij de bevers 95 euro en bij de oudere groepen 150 euro. Hebt u meerdere kinderen die lid zijn van de scouts? Dan gaat er per kind 5 euro van de kostprijs van het kamp (dus de kost is dan 145 euro voor het 2e kind, 140 voor het 3e kind enzovoort)
+                </p>
+            </div>
 
-            <h5>
-                Bivak
-            </h5>
-
-            <p>
-                Al onze takken gaan jaarlijks op weekend, het bivak. Voor de bevers & de welpen is dit samen en valt dit in de paasvakantie. De JGV’s en OGV’s gaan apart en in tenten op bivak.
-            </p>
-
-            <p>
-                Voor alle groepen bedraagt de prijs hiervan typisch rond de 50 euro.
-            </p>
-
-            <h5>
-                Zomerkamp
-            </h5>
-
-            <p>
-                Elke zomer gaan wij met alle groepen samen op kamp. Dit is het hoogtepunt van het jaar en zorgt bij iedereen voor onvergetelijke momenten. De welpen, JGV’s en OGV’s gaan 10 dagen. In het midden is er een dag waarop de ouders verwelkomd worden om het kampterrein te bezoeken, de ouderbezoekdag. Terwijl kunnen de ouders van de bevers hun kinderen brengen. Voor hen begint het kamp dan en duurt dit 5 dagen.
-            </p>
-
-            <p>
-                De prijs hiervoor bedraagt bij de bevers 95 euro en bij de oudere groepen 150 euro. Hebt u meerdere kinderen die lid zijn van de scouts? Dan gaat er per kind 5 euro van de kostprijs van het kamp (dus de kost is dan 145 euro voor het 2e kind, 140 voor het 3e kind enzovoort)
-            </p>
-
-            <h5>
-                Uniform
-            </h5>
-
-            <p>
-                Voor de bevers bestaat het uniform slechts uit een T-shirt en een das. Vanaf de welpen wordt dat T-shirt ingeruild voor een hemd. Het hemd wordt uitgerust met allerlei badges. Sommige badges gaan de hele scoutscarrière mee en anderen behoren enkel tot een specifieke tak. Meer info over het uniform vindt u <a href="https://scoutsoostkamp.be/alle-info/uniform-shop">hier</a>.
-            </p>
+            <div class="section section--small-spacing">
+                <h5>
+                    Uniform
+                </h5>
+                
+                <p>
+                    Voor de bevers bestaat het uniform slechts uit een T-shirt en een das. Vanaf de welpen wordt dat T-shirt ingeruild voor een hemd. Het hemd wordt uitgerust met allerlei badges. Sommige badges gaan de hele scoutscarrière mee en anderen behoren enkel tot een specifieke tak. Meer info over het uniform vindt u <a href="https://scoutsoostkamp.be/alle-info/uniform-shop">hier</a>.
+                </p>
+            </div>
         </div>
     </div>
 
-    <div class="row section">
-        <div class="col-9">
-            <p>
-                Wij doen er alles aan om de kost voor onze werking laag te houden. We beseffen dat dit een grote kost kan zijn, zeker voor grote en kwetsbare gezinnen. Daarom bieden wij enkele mogelijkheden aan om korting aan te vragen op bovenstaande kosten. Deze bestaan uit de <b>SJaCosbonnen</b> van gemeente Oostkamp en het <b>SOMkort</b> en <b>SOMfonds</b>  van FOS Open Scouting.
-            </p>
+    <div class="row section justify-content-center">
+        <div class="col-12 col-md-10 col-lg-8">
+            <div class="section section--small-spacing">
+                <p>
+                    Wij doen er alles aan om de kost voor onze werking laag te houden. We beseffen dat dit een grote kost kan zijn, zeker voor grote en kwetsbare gezinnen. Daarom bieden wij enkele mogelijkheden aan om korting aan te vragen op bovenstaande kosten. Deze bestaan uit de <b>SJaCosbonnen</b> van gemeente Oostkamp en het <b>SOMkort</b> en <b>SOMfonds</b>  van FOS Open Scouting.
+                </p>
+            </div>
 
-            <h5>
-                SJaCosbonnen
-            </h5>
+            <div class="section section--small-spacing">
+                <h5>
+                    SJaCosbonnen
+                </h5>
+                
+                <p>
+                    Ook de gemeente Oostkamp ondersteunt gezinnen met behulp van de SJaCosbonnen. Bij cash betaling van het lidgeld kunt u ons een SJaCosbon geven. De prijs van het lidgeld wordt verminderd met de waarde van uw SJaCosbon. Meer info vindt u <a href="https://www.oostkamp.be/product/241/sjacosbon---vrijetijdsbon-voor-sport-jeugd-en-cultuur">hier</a>.
+                </p>
+            </div>
 
-            <p>
-                Ook de gemeente Oostkamp ondersteunt gezinnen met behulp van de SJaCosbonnen. Bij cash betaling van het lidgeld kunt u ons een SJaCosbon geven. De prijs van het lidgeld wordt verminderd met de waarde van uw SJaCosbon. Meer info vindt u <a href="https://www.oostkamp.be/product/241/sjacosbon---vrijetijdsbon-voor-sport-jeugd-en-cultuur">hier</a>.
-            </p>
+            <div class="section section--small-spacing">
+                <h5>
+                    SOMkort
+                </h5>
+                
+                <p>
+                    Fos Open Scouting biedt voor gezinnen die daar nood aan hebben een korting op het lidgeld aan. Bovenop de korting van FOS geven wij nog een extra korting waardoor zij die hier nood aan hebben slechts de helft van het lidgeld moeten betalen, nog 19 euro dus. Wenst u hier gebruik van te maken? Stort dan 19 i.p.v. 38 euro en vermeld “SOMkort” bij je betaling.
+                </p>
+            </div>
 
-            <h5>
-                SOMkort
-            </h5>
-
-            <p>
-                Fos Open Scouting biedt voor gezinnen die daar nood aan hebben een korting op het lidgeld aan. Bovenop de korting van FOS geven wij nog een extra korting waardoor zij die hier nood aan hebben slechts de helft van het lidgeld moeten betalen, nog 19 euro dus. Wenst u hier gebruik van te maken? Stort dan 19 i.p.v. 38 euro en vermeld “SOMkort” bij je betaling.
-            </p>
-
-            <h5>
-                SOMfonds
-            </h5>
-
-            <p>
-                Aanvullend op het SOMkort-systeem is er ook het SOMfonds systeem. Bij het SOMfonds gaat het om een toelage voor ouders die daar nood aan hebben, voor alles (behalve lidgeld) waar uw kind nood aan heeft om aan scouting te kunnen deelnemen. Denk aan: weekendprijs, kampprijs, unifom, ...
-            </p>
-
-            <p>
-                Wenst u hier gebruik van te maken of meer over te weten? Praat dan met de takleider van je kind of de eenheidsleiding, of stuur een mailtje naar onze email fos207ste@gmail.com.
-            </p>
+            <div class="section section--small-spacing">
+                <h5>
+                    SOMfonds
+                </h5>
+                
+                <p>
+                    Aanvullend op het SOMkort-systeem is er ook het SOMfonds systeem. Bij het SOMfonds gaat het om een toelage voor ouders die daar nood aan hebben, voor alles (behalve lidgeld) waar uw kind nood aan heeft om aan scouting te kunnen deelnemen. Denk aan: weekendprijs, kampprijs, unifom, ...
+                </p>
+                
+                <p>
+                    Wenst u hier gebruik van te maken of meer over te weten? Praat dan met de takleider van je kind of de eenheidsleiding, of stuur een mailtje naar onze email fos207ste@gmail.com.
+                </p>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,4 +1,4 @@
-<section class="section section--small-spacing banner banner--{{ $banner->color }}">
+<section class="section section--extra-small-spacing banner banner--{{ $banner->color }}">
 	<img src="{{ asset('/img/banners/' . $banner->color . '-' . $banner->pattern . '-' . $banner->strength . '.png') }}" alt="{{ $page_title }}" class="banner__image">
 
 	<h1 class="banner__title @if(isset($page_sub_title)) banner__title--with-sub-title @endif">
