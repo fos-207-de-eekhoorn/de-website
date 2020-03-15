ac55668318893b72e8efaf90eee602413561640d
Orry Verpoort <orry@verpoort.be>
2020-03-15 16:08:58 +0100

    Evenementen overview list UI

--- resources/views/evenementen/evenementen.blade.php
+++ resources/views/evenementen/evenementen.blade.php
@@ -15,28 +15,44 @@
         <div class="col-12">
         	<h2>Alle evenementen</h2>
 
-            <ul class="evenementen-list">
+            <ul class="evenementen-list row small-gutters">
                 @foreach($evenementen as $evenement)
-                    <li class="evenementen-list__item evenementen-list-item">
-                        <div class="evenementen-list-item__visual">
-                            <img src="{{ asset('/img/banners/' . $evenement->kleur . '-' . $evenement->banner_patroon . '-' . $evenement->banner_sterkte . '.png') }}" class="evenementen-list-item__image">
+                    <li class="evenementen-list__item col-12 col-md-6">
+                        <a href="#">
+                            <div class="evenementen-list-item">
+                                <div class="evenementen-list-item__visual evenementen-list-item__visual--{{ $evenement->kleur }}">
+                                    <img src="{{ asset('/img/banners/' . $evenement->kleur . '-' . $evenement->banner_patroon . '-' . $evenement->banner_sterkte . '.png') }}" class="evenementen-list-item__image">
 
-                            <p class="evenementen-list-item__date">
-                                <span class="evenementen-list-item__day">
-                                    {{ Carbon\Carbon::parse($evenement->start_datum)->format('j') }}
-                                </span>
+                                    <p class="evenementen-list-item__date">
+                                        <span class="evenementen-list-item__day">
+                                            {{ Carbon\Carbon::parse($evenement->start_datum)->format('j') }}
+                                        </span>
 
-                                <span class="evenementen-list-item__month">
-                                    {{ Carbon\Carbon::parse($evenement->start_datum)->monthName }}
-                                </span>
-                            </p>
-                        </div>
+                                        <span class="evenementen-list-item__month">
+                                            {{ Carbon\Carbon::parse($evenement->start_datum)->monthName }}
+                                        </span>
+                                    </p>
+                                </div>
 
-                        <div class="evenementen-list-item__info">
-                            <h3 class="evenementen-list-item__titel">{{ $evenement->naam }}</h3>
-                            <p class="evenementen-list-item__location">{{ $evenement->locatie }}</p>
-                            <p class="evenementen-list-item__price">{{ $evenement->prijs }}</p>
-                        </div>
+                                <div class="evenementen-list-item__info">
+                                    <h3 class="evenementen-list-item__titel">
+                                        {{ $evenement->naam }}
+                                    </h3>
+
+                                    <p class="evenementen-list-item__text evenementen-list-item__text--location">
+                                        <span class="evenementen-list-item__icon"><i class="fas fa-map-marker-alt"></i></span>{{ $evenement->locatie }}
+                                    </p>
+
+                                    <p class="evenementen-list-item__text evenementen-list-item__text--time">
+                                        <span class="evenementen-list-item__icon"><i class="fas fa-clock"></i></span>Start om {{ Carbon\Carbon::parse($evenement->start_uur)->format('H\ui') }}
+                                    </p>
+
+                                    <p class="evenementen-list-item__text evenementen-list-item__text--price">
+                                        <span class="evenementen-list-item__icon"><i class="fas fa-euro-sign"></i></span>{{ $evenement->prijs }}
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
                     </li>
                 @endforeach
             </ul>
