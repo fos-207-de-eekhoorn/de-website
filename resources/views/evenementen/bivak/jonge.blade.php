dffe727bd4b4426e539e1da4933c767cd75883e8
Orry Verpoort <orry@verpoort.be>
2020-03-19 20:08:24 +0100

    Evenementen pages content update

--- resources/views/evenementen/bivak/jonge.blade.php
+++ resources/views/evenementen/bivak/jonge.blade.php
@@ -8,10 +8,33 @@
             'strength' => 'light',
         ],
         'page_title' => 'Bivak JV/G',
-        'page_sub_title' => '10 tot 13 april',
+        'page_sub_title' => 'Afgelast',
     ])
     @endcomponent
 
+    <div class="row section justify-content-center">
+        <div class="col-5">
+            <h2 class="text--align-center">
+                Met spijt moeten wij meedelen
+            </h2>
+
+            <h4 class="text--align-center">
+                Dat ons bivak van de JG/V's is afgelast wegens het coronavirus.
+
+                Voor meer informatie kan je steeds terecht bij de takleiding van uw kind.
+            </h4>
+
+            <div class="leiding-card-list">
+                @foreach($takleiders as $leider)
+                    @component('components.leiding_card', [
+                        'leider' => $leider,
+                    ])
+                    @endcomponent
+                @endforeach
+            </div>
+        </div>
+    </div>
+    {{--
 	<div class="row section">
         <div class="col-12">
             <h2>Bivak JV/G</h2>
@@ -64,5 +87,6 @@
             <p>Hopelijk tot dan!</p>
             <p>De jonge leiding</p>
         </div>
-    </div>  
+    </div>
+    --}}
 @endsection
