cf6f78df081fe40ac585bcdea283eda1d124c30b
Orry Verpoort <orry@verpoort.be>
2020-07-16 15:03:14 +0200

    Typo in migration file

--- database/migrations/2020_07_16_145723_add_is_activiteit_to_activiteiten.php
+++ database/migrations/2020_07_16_145723_add_is_activiteit_to_activiteiten.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIsActivieitToActiviteiten extends Migration
+class AddIsActiviteitToActiviteiten extends Migration
 {
     /**
      * Run the migrations.
