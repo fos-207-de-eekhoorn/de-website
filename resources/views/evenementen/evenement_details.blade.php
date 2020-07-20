8ad36390fba8a05d239516d2d886a594bd78eef3
Orry Verpoort <orry@verpoort.be>
2020-07-20 09:10:21 +0200

    Banner date fix when date is equals

--- resources/views/evenementen/evenement_details.blade.php
+++ resources/views/evenementen/evenement_details.blade.php
@@ -10,9 +10,9 @@
         'page_title' => $evenement->naam,
         'page_sub_title' => ($evenement->start_datum === $evenement->eind_datum)
             ? Carbon\Carbon::parse($evenement->start_datum)->format('j') . ' ' . Carbon\Carbon::parse($evenement->start_datum)->monthName
-            : (Carbon\Carbon::parse($evenement->start_datum)->monthName === Carbon\Carbon::parse($evenement->eind_datum)->monthName)
+            : ((Carbon\Carbon::parse($evenement->start_datum)->monthName === Carbon\Carbon::parse($evenement->eind_datum)->monthName)
                 ? Carbon\Carbon::parse($evenement->start_datum)->format('j') . ' - ' . Carbon\Carbon::parse($evenement->eind_datum)->format('j') . ' ' . Carbon\Carbon::parse($evenement->start_datum)->monthName
-                : Carbon\Carbon::parse($evenement->start_datum)->format('j') . ' ' . Carbon\Carbon::parse($evenement->start_datum)->monthName . ' - ' . Carbon\Carbon::parse($evenement->eind_datum)->format('j') . ' ' . Carbon\Carbon::parse($evenement->eind_datum)->monthName
+                : Carbon\Carbon::parse($evenement->start_datum)->format('j') . ' ' . Carbon\Carbon::parse($evenement->start_datum)->monthName . ' - ' . Carbon\Carbon::parse($evenement->eind_datum)->format('j') . ' ' . Carbon\Carbon::parse($evenement->eind_datum)->monthName)
     ])
     @endcomponent
 
@@ -23,7 +23,6 @@
             <h5>Datum</h5>
             <p>
                 {{ $evenement->start_datum }} - {{ $evenement->eind_datum }}
-                {{ $evenement->start_datum == $evenement->eind_datum ? "True" : "False" }}
             </p>
 
             <h5>Tijd</h5>
