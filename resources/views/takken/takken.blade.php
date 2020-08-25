0b7e5cd35a3eaf5143cfbdb45fb12d44e29bbb04
Orry Verpoort <orry@verpoort.be>
2020-08-25 19:24:01 +0200

    Mobile enhancements

--- resources/views/takken/takken.blade.php
+++ resources/views/takken/takken.blade.php
@@ -14,13 +14,13 @@
     @foreach($takken as $tak)
         <div class="tak section section--small-spacing">
             <div class="row">
-                <div class="col-3">
+                <div class="col-12 col-md-3">
                     <div class="tak__img-wrapper">
                         <img src="/img/{{ $tak->foto }}" alt="{{ $tak->naam }}" class="tak__img">
                     </div>
                 </div>
 
-                <div class="col-9">
+                <div class="col-12 col-md-9">
                     <div class="tak__info cs-{{ $tak->kleur }}">
                         <header class="tak__header">
                             <h3 class="tak__titel">{{ $tak->naam }}</h3>
