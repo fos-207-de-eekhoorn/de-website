60f73a5c1645d3dc9d90b59fdce870c34b2d67ab
Orry Verpoort <orry@verpoort.be>
2020-09-21 22:05:28 +0200

    Rename migration file

--- database/migrations/2020_09_21_171318_create_contents_table.php
+++ database/migrations/2020_09_21_171318_create_contents_table.php
@@ -18,6 +18,7 @@ class CreateContentsTable extends Migration
             $table->string('key');
             $table->string('name');
             $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
