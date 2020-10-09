94432539f12f8ebfc881e48f09bd4cbe954442d3
Orry Verpoort <orry@verpoort.be>
2020-10-09 14:44:37 +0200

    Make verhuurlijst table responsive

--- resources/views/alle-info/verhuurlijst.blade.php
+++ resources/views/alle-info/verhuurlijst.blade.php
@@ -20,83 +20,86 @@
             <div class="row">
                 <div class="col-12 col-lg-8">
                     <h3>Materiaal</h3>
-                    <table class="table">
-                        <thead>
-                            <tr class="table__row">
-                                <td class="table__cell">Goodies</td>
-                                <td class="table__cell">Prijs/dag</td>
-                                <td class="table__cell">Waarborg</td>
-                            </tr>
-                        </thead>
 
-                        <tbody>
-                            <tr class="table__row">
-                                <td class="table__cell">Kommel (scheepstouw)</td>
-                                <td class="table__cell">€25</td>
-                                <td class="table__cell">€25</td>
-                            </tr>
-                            <tr class="table__row">
-                                <td class="table__cell">BBQ</td>
-                                <td class="table__cell">€25</td>
-                                <td class="table__cell">€25</td>
-                            </tr>
-                            <tr class="table__row">
-                                <td class="table__cell">100 tal borden, kommen, bestek, ...</td>
-                                <td class="table__cell">€10</td>
-                                <td class="table__cell">€10</td>
-                            </tr>
-                            <tr class="table__row">
-                                <td class="table__cell">Sjorbalken (groot en klein)</td>
-                                <td class="table__cell">€20</td>
-                                <td class="table__cell">€100</td>
-                            </tr>
-                            <tr class="table__row">
-                                <td class="table__cell">Seniortent</td>
-                                <td class="table__cell">€50</td>
-                                <td class="table__cell">€100</td>
-                            </tr>
-                            <tr class="table__row">
-                                <td class="table__cell">Bartent</td>
-                                <td class="table__cell">€50</td>
-                                <td class="table__cell">€500</td>
-                            </tr>
-                            <tr class="table__row">
-                                <td class="table__cell">Friteuse</td>
-                                <td class="table__cell">€25</td>
-                                <td class="table__cell">€25</td>
-                            </tr>
-                            <tr class="table__row">
-                                <td class="table__cell">Tafels</td>
-                                <td class="table__cell">€10</td>
-                                <td class="table__cell">€10</td>
-                            </tr>
-                            <tr class="table__row">
-                                <td class="table__cell">Banken</td>
-                                <td class="table__cell">€10</td>
-                                <td class="table__cell">€10</td>
-                            </tr>
-                            <tr class="table__row">
-                                <td class="table__cell">Gasbekken + gasfles</td>
-                                <td class="table__cell">€25</td>
-                                <td class="table__cell">€25</td>
-                            </tr>
-                            <tr class="table__row">
-                                <td class="table__cell">Alpinotent</td>
-                                <td class="table__cell">€20</td>
-                                <td class="table__cell">€50</td>
-                            </tr>
-                            <tr class="table__row">
-                                <td class="table__cell">Grondzeil (rood)</td>
-                                <td class="table__cell">€25</td>
-                                <td class="table__cell">€25</td>
-                            </tr>
-                            <tr class="table__row">
-                                <td class="table__cell">Grondboor</td>
-                                <td class="table__cell">€10</td>
-                                <td class="table__cell">€10</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div class="wrapper__table">
+                        <table class="table">
+                            <thead>
+                                <tr class="table__row">
+                                    <td class="table__cell">Goodies</td>
+                                    <td class="table__cell">Prijs/dag</td>
+                                    <td class="table__cell">Waarborg</td>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr class="table__row">
+                                    <td class="table__cell">Kommel (scheepstouw)</td>
+                                    <td class="table__cell">€25</td>
+                                    <td class="table__cell">€25</td>
+                                </tr>
+                                <tr class="table__row">
+                                    <td class="table__cell">BBQ</td>
+                                    <td class="table__cell">€25</td>
+                                    <td class="table__cell">€25</td>
+                                </tr>
+                                <tr class="table__row">
+                                    <td class="table__cell">100 tal borden, kommen, bestek, ...</td>
+                                    <td class="table__cell">€10</td>
+                                    <td class="table__cell">€10</td>
+                                </tr>
+                                <tr class="table__row">
+                                    <td class="table__cell">Sjorbalken (groot en klein)</td>
+                                    <td class="table__cell">€20</td>
+                                    <td class="table__cell">€100</td>
+                                </tr>
+                                <tr class="table__row">
+                                    <td class="table__cell">Seniortent</td>
+                                    <td class="table__cell">€50</td>
+                                    <td class="table__cell">€100</td>
+                                </tr>
+                                <tr class="table__row">
+                                    <td class="table__cell">Bartent</td>
+                                    <td class="table__cell">€50</td>
+                                    <td class="table__cell">€500</td>
+                                </tr>
+                                <tr class="table__row">
+                                    <td class="table__cell">Friteuse</td>
+                                    <td class="table__cell">€25</td>
+                                    <td class="table__cell">€25</td>
+                                </tr>
+                                <tr class="table__row">
+                                    <td class="table__cell">Tafels</td>
+                                    <td class="table__cell">€10</td>
+                                    <td class="table__cell">€10</td>
+                                </tr>
+                                <tr class="table__row">
+                                    <td class="table__cell">Banken</td>
+                                    <td class="table__cell">€10</td>
+                                    <td class="table__cell">€10</td>
+                                </tr>
+                                <tr class="table__row">
+                                    <td class="table__cell">Gasbekken + gasfles</td>
+                                    <td class="table__cell">€25</td>
+                                    <td class="table__cell">€25</td>
+                                </tr>
+                                <tr class="table__row">
+                                    <td class="table__cell">Alpinotent</td>
+                                    <td class="table__cell">€20</td>
+                                    <td class="table__cell">€50</td>
+                                </tr>
+                                <tr class="table__row">
+                                    <td class="table__cell">Grondzeil (rood)</td>
+                                    <td class="table__cell">€25</td>
+                                    <td class="table__cell">€25</td>
+                                </tr>
+                                <tr class="table__row">
+                                    <td class="table__cell">Grondboor</td>
+                                    <td class="table__cell">€10</td>
+                                    <td class="table__cell">€10</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
 
                 <div class="col-12 col-lg-4">
