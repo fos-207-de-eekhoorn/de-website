b9092647357e990bc950d8cae128d4b0ed1b7980
Orry Verpoort <orry@verpoort.be>
2021-02-20 15:28:42 +0100

    Warning message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,21 @@
         @include('layouts.app_components.header')
 
         <main class="main">
+            <div class="container page-warning">
+                <div class="page-warning__icon icon">
+                    <i class="fas fa-exclamation-triangle"></i>
+                </div>
+
+                <p class="page-warning__warning text--align-center">
+                    Dit is een testversie van onze website.<br>
+                    De informatie die hier te vinden is, is NIET actueel of juist.
+                </p>
+
+                <div class="page-warning__icon icon">
+                    <i class="fas fa-exclamation-triangle"></i>
+                </div>
+            </div>
+
             <div class="container cs-white shadow">
                 <div class="main__inner">
                     @yield('content')
